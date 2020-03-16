0da8b02674e61b5d012fb22b489ed6efe7a3a897
Ravi Thakur <43831111+ravithakur2k@users.noreply.github.com>
2020-03-16 00:17:35 -0400

    Get and Post Request
    Learnt how to get data from server through API call and also to post data to server

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 
 import './FullPost.css';
+import Axios from 'axios';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
 
-        );
+    state = {
+        loadedPost: null
+    }
+
+    componentDidUpdate() {
+        if (this.props.id) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                Axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(Response => {
+                    console.log(Response)
+                    this.setState({
+                        loadedPost: Response.data
+                    })
+                })
+            }
+        }
+    }
+    render() {
+        const loadedPost = this.state.loadedPost
+        let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+        if (this.props.id) {
+            post = <p style={{ textAlign: "center" }}>Loading...</p>;
+        }
+        if (this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{loadedPost.title}</h1>
+                    <p>{loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+            );
+        }
         return post;
     }
 }
